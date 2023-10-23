9b83ea3f7db9af9b55ca484cd7ed5811cbd06ce0
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-22 21:28:12 -0400

    pass tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode='r', encoding="utf-8") as file:
+        return file.read()
